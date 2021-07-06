315022b614b036728bfba45a58d2335ad7fd494b
vgoylo <y_draiv.by@mail.ru>
2021-07-06 16:15:29 +0300

    Initial commit

--- str_hash.rb
+++ str_hash.rb
@@ -1,11 +1,11 @@
 def s_hash(str)
   hash = {} 
-  arr = str.split(/[^[[:word:]]]+/)
+  arr = str.split(' ')
   arr.each do |element|
     hash[element] = arr.count(element)
   end
 
-  hash
+  hash.sort_by {|_key, value| value}.reverse.to_h
 end
 
-puts s_hash('Тогда лучше идите я как выйдете из машины напишите Тогда лучше идите ')
+p s_hash('Тогда лучше лучше лучше идите я is is is как выйдете выйдете из машины напишите is is is Тогда лучше идите ')
